6730c0e2b4f65354efee5bc263162d014cf58987
pavan <pavankoneti1@gmail.com>
2023-06-07 12:18:10 +0530

    deploy

--- project/project/settings.py
+++ project/project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
